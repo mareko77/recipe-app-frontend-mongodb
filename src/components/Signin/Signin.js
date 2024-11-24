ed7823445c46e0c8024542ca841697271effb5ac
Marek Ograbek <36967486+mareko77@users.noreply.github.com>
2024-11-24 22:29:34 +0000

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
       onSubmitSignIn = () => {
-        fetch('http://localhost:3002/signin', {
+        fetch('https://recipe-app-backend-mongodb.onrender.com/signin', {
           method: 'post',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
