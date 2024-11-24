a0d9e2c7119b06bba4e70639e7233857784a2ddd
Marek Ograbek <36967486+mareko77@users.noreply.github.com>
2024-11-24 22:28:58 +0000

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3002/register', {
+    fetch('https://recipe-app-backend-mongodb.onrender.com/register', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
